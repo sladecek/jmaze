cec0dc6e194435b6325be1da751cf2a9a63745b2
Ladislav Sladecek <ladislav.sladecek@gmail.com>
2015-04-26 09:58:34 +0200

    Initial commit

---
+++ src/main/java/com/github/sladecek/maze/jmaze/App.java
@@ -0,0 +1,14 @@
+package com.github.sladecek.maze.jmaze;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
