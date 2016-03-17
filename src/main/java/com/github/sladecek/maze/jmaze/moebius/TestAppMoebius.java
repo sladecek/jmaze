bba7632044cda39bb0bb611d9da4dbcadc7dfa8b
Ladislav Sladecek <ladislav.sladecek@gmail.com>
2016-03-17 12:07:09 +0100

    exception handling - cleanup

--- src/main/java/com/github/sladecek/maze/jmaze/moebius/TestAppMoebius.java
+++ src/main/java/com/github/sladecek/maze/jmaze/moebius/TestAppMoebius.java
@@ -1,8 +1,12 @@
 package com.github.sladecek.maze.jmaze.moebius;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.Random;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import com.github.sladecek.maze.jmaze.generator.DepthFirstMazeGenerator;
 import com.github.sladecek.maze.jmaze.generator.IMazeGenerator;
@@ -13,37 +17,45 @@ import com.github.sladecek.maze.jmaze.print3d.ThreeJs3DPrinter;
 import com.github.sladecek.maze.jmaze.printstyle.DefaultPrintStyle;
 import com.github.sladecek.maze.jmaze.printstyle.IPrintStyle;
 import com.github.sladecek.maze.jmaze.shapes.ShapeContainer;
-import com.github.sladecek.maze.jmaze.util.MazeGenerationException;
 
 /***
  * Command line application generating a Moebius maze into OpenScad file.
  */
 public final class TestAppMoebius {
+
+	private static final Logger LOG = Logger.getLogger("maze.jmaze");
+
 	public static void main(final String[] args) {
-		final int widthCells = 8;
-		final int lengthCells = 120;
-		MoebiusMaze maze = new MoebiusMaze(widthCells, lengthCells);
-		final Random randomGenerator = new Random();
-		randomGenerator.setSeed(0);
-		IMazeGenerator g = new DepthFirstMazeGenerator(randomGenerator);
-		MazeRealization r = g.generateMaze(maze);
 
-		Maze3DSizes sizes = new Maze3DSizes();
-		sizes.setCellSizeInmm(2);
-
-		IPrintStyle colors = new DefaultPrintStyle();
-
-		ShapeContainer shapes =  maze.applyRealization(r);
-		
-		double approxRoomSizeInmm = 3;
-		MoebiusBlockMaker maker = new MoebiusBlockMaker(shapes, sizes, colors,
-				approxRoomSizeInmm);
-
-		final boolean printInJs = true;
-		final boolean printInScad = true;
-		final boolean showSolution = true;
-
+		LogManager.getLogManager().reset();
+		LOG.setLevel(Level.INFO);
 		try {
+			FileHandler fh = new FileHandler("maze.log");
+			LOG.addHandler(fh);
+			fh.setFormatter(new SimpleFormatter());
+
+			final int widthCells = 8;
+			final int lengthCells = 120;
+			MoebiusMaze maze = new MoebiusMaze(widthCells, lengthCells);
+			final Random randomGenerator = new Random();
+			randomGenerator.setSeed(0);
+			IMazeGenerator g = new DepthFirstMazeGenerator(randomGenerator);
+			MazeRealization r = g.generateMaze(maze);
+
+			Maze3DSizes sizes = new Maze3DSizes();
+			sizes.setCellSizeInmm(2);
+
+			IPrintStyle colors = new DefaultPrintStyle();
+
+			ShapeContainer shapes = maze.applyRealization(r);
+
+			double approxRoomSizeInmm = 3;
+			MoebiusBlockMaker maker = new MoebiusBlockMaker(shapes, sizes, colors, approxRoomSizeInmm);
+
+			final boolean printInJs = true;
+			final boolean printInScad = true;
+			final boolean showSolution = true;
+
 			if (printInJs) {
 				FileOutputStream f;
 				f = new FileOutputStream("maze-moebius.js");
@@ -54,7 +66,7 @@ public final class TestAppMoebius {
 				new OpenScad3DPrinter().printBlocks(maker, showSolution, f);
 			}
 
-		} catch (FileNotFoundException | MazeGenerationException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
