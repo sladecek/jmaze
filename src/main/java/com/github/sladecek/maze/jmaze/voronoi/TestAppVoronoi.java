c56e3bc4dc452cd760c1b63af638ed04f851ef02
Ladislav Sladecek <ladislav.sladecek@gmail.com>
2016-08-29 13:16:50 +0200

    PolygonSeparator WIP

--- src/main/java/com/github/sladecek/maze/jmaze/voronoi/TestAppVoronoi.java
+++ src/main/java/com/github/sladecek/maze/jmaze/voronoi/TestAppVoronoi.java
@@ -5,17 +5,14 @@ import java.util.Random;
 import com.github.sladecek.maze.jmaze.print2d.TestApp2DBase;
 
 public class TestAppVoronoi extends TestApp2DBase {
-
 	
 	public static void main(String[] args) {
 		new TestAppVoronoi().printTestMaze("maze-voronoi", () -> {
 			final int width = 10;
 			final int height = 10;
-			final int roomCount = 3;
+			final int roomCount = 4;
 			final int loydCount = 1;
-					
-					
-					
+														
 			final Random randomGenerator = new Random();
 			randomGenerator.setSeed(4);
 			return  new Voronoi2DMaze(width, height, roomCount, loydCount, randomGenerator, true);
