445fb8da840b4697e413be274073073a7b44bb88
Ladislav Sladecek <ladislav.ladecek@gmail.com>
2017-02-10 16:36:19 +0100

    Cleanup.

--- src/main/java/com/github/sladecek/maze/jmaze/voronoi/TestAppVoronoi.java
+++ src/main/java/com/github/sladecek/maze/jmaze/voronoi/TestAppVoronoi.java
@@ -4,19 +4,28 @@ import java.util.Random;
 
 import com.github.sladecek.maze.jmaze.print2d.TestApp2DBase;
 
+/**
+ * Ad-hoc test of Voronou maze.
+ */
 public class TestAppVoronoi extends TestApp2DBase {
 
-	public static void main(String[] args) {
-		new TestAppVoronoi().printTestMaze("maze-voronoi", () -> {
-			final int width = 10;
-			final int height = 10;
-			final int roomCount = 100;
-			final int loydCount = 7;
+    /**
+     * Main method.
+     *
+     * @param args Program arguments.
+     */
+    public static void main(final String[] args) {
+        new TestAppVoronoi().printTestMaze("maze-voronoi", () -> {
+            final int width = 10;
+            final int height = 10;
+            final int roomCount = 100;
+            final int loydCount = 10;
+            final int seed = 4;
 
-			final Random randomGenerator = new Random();
-			randomGenerator.setSeed(4);
-			return new Voronoi2DMaze(width, height, roomCount, loydCount, randomGenerator, true);
-		});
-	}
+            final Random randomGenerator = new Random();
+            randomGenerator.setSeed(seed);
+            return new Voronoi2DMaze(width, height, roomCount, loydCount, randomGenerator, true);
+        });
+    }
 
 }
