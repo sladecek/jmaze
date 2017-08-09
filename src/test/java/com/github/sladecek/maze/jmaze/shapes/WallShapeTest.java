2a5715b7a7af9cecf837679dfd1c19acc74739a2
Ladislav Sladecek <ladislav.ladecek@gmail.com>
2017-08-09 13:42:42 +0200

    WallShapeTest

--- src/test/java/com/github/sladecek/maze/jmaze/shapes/WallShapeTest.java
+++ src/test/java/com/github/sladecek/maze/jmaze/shapes/WallShapeTest.java
@@ -1,222 +1,215 @@
 package com.github.sladecek.maze.jmaze.shapes;
 
+import com.github.sladecek.maze.jmaze.geometry.Point2DInt;
+import com.github.sladecek.maze.jmaze.print2d.I2DDocument;
+import com.github.sladecek.maze.jmaze.printstyle.Color;
+import com.github.sladecek.maze.jmaze.printstyle.IPrintStyle;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 
 public class WallShapeTest {
 
-/* TODO opravit
-	@Test
-	public void testGetShapeType() {
-		WallShape w = new WallShape(WallType.hole, 1, 2, 3, 4);
-		assertEquals(WallType.hole, w.getShapeType());
-	}
 
-	@Test
-	public void whenCoordinatesAreRectangularTestPrint2DShouldPrintLine() {
-		I2DDocument mockedDocument = mock(I2DDocument.class);
-		
-		boolean isPolarCoordinates = false;
-		WallType shapeType = WallType.hole;
-		callPrint2D(mockedDocument, isPolarCoordinates, shapeType, 3, 4);
-		
-		ArgumentCaptor<Point2DInt> p1 = ArgumentCaptor.forClass(Point2DInt.class);
-		ArgumentCaptor<Point2DInt> p2 = ArgumentCaptor.forClass(Point2DInt.class);
-		ArgumentCaptor<String> style = ArgumentCaptor.forClass(String.class);
+    @Test
+    public void testGetShapeType() {
+        final int id = 777;
+        WallShape w = new WallShape(id, WallType.innerWall, new Point2DInt(11, 22), new Point2DInt(33, 44), 3, 4);
+        assertEquals(WallType.innerWall, w.getWallType());
+    }
 
-		verify(mockedDocument).printLine(p1.capture(), p2.capture(), style.capture());
-		
+    @Test
+    public void whenCoordinatesAreRectangularTestPrint2DShouldPrintLine() {
+        I2DDocument mockedDocument = mock(I2DDocument.class);
 
-		assertEquals(0, p1.getValue().getX());
-		assertEquals(1, p1.getValue().getY());
-		
-		assertEquals(4, p2.getValue().getX());
-		assertEquals(3, p2.getValue().getY());
-		
-		assertEquals("", style.getValue());
+        boolean isPolarCoordinates = false;
+        WallType shapeType = WallType.innerWall;
+        callPrint2D(mockedDocument, isPolarCoordinates, shapeType, 3, 4);
 
-	}
+        ArgumentCaptor<Point2DInt> p1 = ArgumentCaptor.forClass(Point2DInt.class);
+        ArgumentCaptor<Point2DInt> p2 = ArgumentCaptor.forClass(Point2DInt.class);
+        ArgumentCaptor<String> style = ArgumentCaptor.forClass(String.class);
 
-	@Test
-	public void whenCoordinatesArePolarTestPrint2DShouldPrintRadialLine() {
-		I2DDocument mockedDocument = mock(I2DDocument.class);
-		
-		boolean isPolarCoordinates = true;
-		WallType shapeType = WallType.hole;
-		callPrint2D(mockedDocument, isPolarCoordinates, shapeType, 5, 0);
-		
-		ArgumentCaptor<Point2DInt> p1 = ArgumentCaptor.forClass(Point2DInt.class);
-		ArgumentCaptor<Point2DInt> p2 = ArgumentCaptor.forClass(Point2DInt.class);
-		ArgumentCaptor<String> style = ArgumentCaptor.forClass(String.class);
+        verify(mockedDocument).printLine(p1.capture(), p2.capture(), style.capture());
 
-		verify(mockedDocument).printLine(p1.capture(), p2.capture(), style.capture());
-		
 
-		assertEquals(0, p1.getValue().getX());
-		assertEquals(1, p1.getValue().getY());
-		
-		assertEquals(0, p2.getValue().getX());
-		assertEquals(5, p2.getValue().getY());
-		
-		assertEquals("", style.getValue());
+        assertEquals(0, p1.getValue().getX());
+        assertEquals(1, p1.getValue().getY());
 
-	}
+        assertEquals(4, p2.getValue().getX());
+        assertEquals(3, p2.getValue().getY());
 
-	@Test
-	public void whenCoordinatesArePolarTestPrint2DShouldPrintArc() {
-		I2DDocument mockedDocument = mock(I2DDocument.class);
-		
-		boolean isPolarCoordinates = true;
-		WallType shapeType = WallType.hole;
-		callPrint2D(mockedDocument, isPolarCoordinates, shapeType, 1, 5);
-		
-		ArgumentCaptor<Point2DInt> p1 = ArgumentCaptor.forClass(Point2DInt.class);
-		ArgumentCaptor<Point2DInt> p2 = ArgumentCaptor.forClass(Point2DInt.class);
-		ArgumentCaptor<String> style = ArgumentCaptor.forClass(String.class);
-		verify(mockedDocument).printArcSegment(p1.capture(), p2.capture(), style.capture());
-		
+        assertEquals("stroke:rgb(4,4,4);stroke-width:3", style.getValue());
 
-		assertEquals(0, p1.getValue().getX());
-		assertEquals(1, p1.getValue().getY());
-		
-		assertEquals(5, p2.getValue().getX());
-		assertEquals(1, p2.getValue().getY());
-		
-		assertEquals("", style.getValue());		
-	}
+    }
 
-	@Test
-	public void whenCoordinatesArePolarTestPrint2DShouldPrintCircle() {
-		I2DDocument mockedDocument = mock(I2DDocument.class);
-		
-		boolean isPolarCoordinates = true;
-		WallType shapeType = WallType.hole;
-		callPrint2D(mockedDocument, isPolarCoordinates, shapeType, 1, 0);
-		
-		ArgumentCaptor<Point2DInt> center = ArgumentCaptor.forClass(Point2DInt.class);
-		ArgumentCaptor<String> fill = ArgumentCaptor.forClass(String.class);
-		ArgumentCaptor<String> style = ArgumentCaptor.forClass(String.class);
-		ArgumentCaptor<Integer> perimeter = ArgumentCaptor.forClass(Integer.class);
-		ArgumentCaptor<Boolean> isPerimeterAbsolute = ArgumentCaptor.forClass(Boolean.class);
-		
-		verify(mockedDocument).printCircle(center.capture(), fill.capture(), 
-				perimeter.capture(),
-				isPerimeterAbsolute.capture(), style.capture());
-		
+    @Test
+    public void whenCoordinatesArePolarTestPrint2DShouldPrintRadialLine() {
+        I2DDocument mockedDocument = mock(I2DDocument.class);
 
-		assertEquals(0, center.getValue().getX());
-		assertEquals(0, center.getValue().getY());
-		assertEquals("none", fill.getValue());
-		assertEquals("", style.getValue());
-		assertEquals(1, (int)perimeter.getValue());
-		assertEquals(false, isPerimeterAbsolute.getValue());		
-	}
-	
-	@Test
-	public void testPrint2DShouldPrintInnerWallInCorrectStyle() {		
-		String expectedStyle = "stroke:rgb(4,4,4);stroke-width:3";   
-		boolean expectedCenter = false;
-		WallType shapeType = WallType.innerWall;
-		test2DPrintStyle(expectedStyle, expectedCenter, shapeType);		
-	}
+        boolean isPolarCoordinates = true;
+        WallType shapeType = WallType.innerWall;
+        callPrint2D(mockedDocument, isPolarCoordinates, shapeType, 5, 0);
 
-	@Test
-	public void testPrint2DShouldPrintOuterWallInCorrectStyle() {		
-		String expectedStyle = "stroke:rgb(5,5,5);stroke-width:5";   
-		boolean expectedCenter = false;
-		WallType shapeType = WallType.outerWall;
-		test2DPrintStyle(expectedStyle, expectedCenter, shapeType);		
-	}
+        ArgumentCaptor<Point2DInt> p1 = ArgumentCaptor.forClass(Point2DInt.class);
+        ArgumentCaptor<Point2DInt> p2 = ArgumentCaptor.forClass(Point2DInt.class);
+        ArgumentCaptor<String> style = ArgumentCaptor.forClass(String.class);
 
-	@Test
-	public void testPrint2DShouldPrintSolutionWallInCorrectStyle() {		
-		String expectedStyle = "stroke:rgb(6,6,6);stroke-width:7";   
-		boolean expectedCenter = true;
-		WallType shapeType = WallType.solution;
-		test2DPrintStyle(expectedStyle, expectedCenter, shapeType);		
-	}
+        verify(mockedDocument).printLine(p1.capture(), p2.capture(), style.capture());
 
-	@Test
-	public void testPrint2DShouldPrintAuxiliaryWallInCorrectStyle() {		
-		String expectedStyle = "stroke:rgb(7,7,7);stroke-width:3";   
-		boolean expectedCenter = false;
-		WallType shapeType = WallType.auxiliaryWall;
-		test2DPrintStyle(expectedStyle, expectedCenter, shapeType);		
-	}
+        assertEquals(0, p1.getValue().getX());
+        assertEquals(1, p1.getValue().getY());
 
-	private void test2DPrintStyle(String expectedStyle, boolean expectedCenter,
-			WallType shapeType) {
-		I2DDocument mockedDocument = mock(I2DDocument.class);
-		boolean isPolarCoordinates = false;
-		callPrint2D(mockedDocument, isPolarCoordinates, shapeType, 3, 0);
-		
-		ArgumentCaptor<Point2DInt> p1 = ArgumentCaptor.forClass(Point2DInt.class);
-		ArgumentCaptor<Point2DInt> p2 = ArgumentCaptor.forClass(Point2DInt.class);
-		ArgumentCaptor<String> style = ArgumentCaptor.forClass(String.class);
+        assertEquals(0, p2.getValue().getX());
+        assertEquals(5, p2.getValue().getY());
 
-		verify(mockedDocument).printLine(p1.capture(), p2.capture(), style.capture());
-				
-		assertEquals(expectedStyle, style.getValue());
+        assertEquals("stroke:rgb(4,4,4);stroke-width:3", style.getValue());
 
-	}
+    }
 
-	
-	private void callPrint2D(I2DDocument mockedDocument, boolean isPolarCoordinates, WallType shapeType, int y2, int x2) {
-		int pictureHeight = 100;
-		int pictureWidth = 200;
-		int resolutionX = 7;
-		int resolutionY = 7;
-		ShapeContext sc = new ShapeContext(isPolarCoordinates, pictureHeight, pictureWidth);
-		
-		int y1 = 1;
-		int x1 = 0;
-		
-		when(mockedDocument.getContext()).thenReturn(sc);		
-		WallShape w = new WallShape(shapeType, y1, x1, y2, x2);
-		
-		IPrintStyle mockedPrintStyle = mock(IPrintStyle.class);
-		when(mockedPrintStyle.getHoleColor()).thenReturn(new Color("010203"));
-		when(mockedPrintStyle.getInnerWallColor()).thenReturn(new Color("040404"));
-		when(mockedPrintStyle.getOuterWallColor()).thenReturn(new Color("050505"));
-		when(mockedPrintStyle.getSolutionWallColor()).thenReturn(new Color("060606"));
-		when(mockedPrintStyle.getDebugWallColor()).thenReturn(new Color("070707"));
-		when(mockedPrintStyle.getInnerWallWidth()).thenReturn(3);
-		when(mockedPrintStyle.getOuterWallWidth()).thenReturn(5);
-		when(mockedPrintStyle.getSolutionWallWidth()).thenReturn(7);
-		
-		w.print2D(mockedDocument, mockedPrintStyle);
-	}
+    @Test
+    public void whenCoordinatesArePolarTestPrint2DShouldPrintArc() {
+        I2DDocument mockedDocument = mock(I2DDocument.class);
 
-	@Test
-	public void testGetX1() {
-		WallShape w = new WallShape(WallType.hole, 1, 2, 3, 4);
-		assertEquals(2, w.getX1());
-	}
+        boolean isPolarCoordinates = true;
+        WallType shapeType = WallType.innerWall;
+        callPrint2D(mockedDocument, isPolarCoordinates, shapeType, 1, 5);
 
-	@Test
-	public void testGetX2() {
-		WallShape w = new WallShape(WallType.hole, 1, 2, 3, 4);
-		assertEquals(4, w.getX2());
-	}
+        ArgumentCaptor<Point2DInt> p1 = ArgumentCaptor.forClass(Point2DInt.class);
+        ArgumentCaptor<Point2DInt> p2 = ArgumentCaptor.forClass(Point2DInt.class);
+        ArgumentCaptor<String> style = ArgumentCaptor.forClass(String.class);
+        verify(mockedDocument).printArcSegment(p1.capture(), p2.capture(), style.capture());
 
-	@Test
-	public void testGetY1() {
-		WallShape w = new WallShape(WallType.hole, 1, 2, 3, 4);
-		assertEquals(1, w.getY1());
-	}
 
-	@Test
-	public void testGetY2() {
-		WallShape w = new WallShape(WallType.hole, 1, 2, 3, 4);
-		assertEquals(3, w.getY2());
-	}
+        assertEquals(0, p1.getValue().getX());
+        assertEquals(1, p1.getValue().getY());
 
-	@Test
-	public void testToString() {
-		WallShape w = new WallShape(WallType.hole, 1, 2, 3, 4);
-		assertEquals("WallShape [ shapeType=hole, x1=2, x2=4, y1=1, y2=3]", w.toString());
-	}
-*/
+        assertEquals(5, p2.getValue().getX());
+        assertEquals(1, p2.getValue().getY());
+
+        assertEquals("stroke:rgb(4,4,4);stroke-width:3", style.getValue());
+    }
+
+    @Test
+    public void whenCoordinatesArePolarTestPrint2DShouldPrintCircle() {
+        I2DDocument mockedDocument = mock(I2DDocument.class);
+
+        boolean isPolarCoordinates = true;
+        WallType shapeType = WallType.innerWall;
+        callPrint2D(mockedDocument, isPolarCoordinates, shapeType, 1, 0);
+
+        ArgumentCaptor<Point2DInt> center = ArgumentCaptor.forClass(Point2DInt.class);
+        ArgumentCaptor<String> fill = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> style = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<Integer> perimeter = ArgumentCaptor.forClass(Integer.class);
+        ArgumentCaptor<Boolean> isPerimeterAbsolute = ArgumentCaptor.forClass(Boolean.class);
+
+        verify(mockedDocument).printCircle(center.capture(), fill.capture(),
+                perimeter.capture(),
+                isPerimeterAbsolute.capture(), style.capture());
+
+
+        assertEquals(0, center.getValue().getX());
+        assertEquals(0, center.getValue().getY());
+        assertEquals("none", fill.getValue());
+        assertEquals("stroke:rgb(4,4,4);stroke-width:3", style.getValue());
+        assertEquals(1, (int) perimeter.getValue());
+        assertEquals(false, isPerimeterAbsolute.getValue());
+    }
+
+    @Test
+    public void testPrint2DShouldPrintInnerWallInCorrectStyle() {
+        String expectedStyle = "stroke:rgb(4,4,4);stroke-width:3";
+        boolean expectedCenter = false;
+        WallType shapeType = WallType.innerWall;
+        test2DPrintStyle(expectedStyle, expectedCenter, shapeType);
+    }
+
+    @Test
+    public void testPrint2DShouldPrintOuterWallInCorrectStyle() {
+        String expectedStyle = "stroke:rgb(5,5,5);stroke-width:5";
+        boolean expectedCenter = false;
+        WallType shapeType = WallType.outerWall;
+        test2DPrintStyle(expectedStyle, expectedCenter, shapeType);
+    }
+
+    @Test
+    public void testPrint2DShouldPrintSolutionWallInCorrectStyle() {
+        String expectedStyle = "stroke:rgb(5,5,5);stroke-width:5";
+        boolean expectedCenter = true;
+        WallType shapeType = WallType.outerWall;
+        test2DPrintStyle(expectedStyle, expectedCenter, shapeType);
+    }
+
+    private void test2DPrintStyle(String expectedStyle, boolean expectedCenter,
+                                  WallType shapeType) {
+        I2DDocument mockedDocument = mock(I2DDocument.class);
+        boolean isPolarCoordinates = false;
+        callPrint2D(mockedDocument, isPolarCoordinates, shapeType, 3, 0);
+
+        ArgumentCaptor<Point2DInt> p1 = ArgumentCaptor.forClass(Point2DInt.class);
+        ArgumentCaptor<Point2DInt> p2 = ArgumentCaptor.forClass(Point2DInt.class);
+        ArgumentCaptor<String> style = ArgumentCaptor.forClass(String.class);
+
+        verify(mockedDocument).printLine(p1.capture(), p2.capture(), style.capture());
+
+        assertEquals(expectedStyle, style.getValue());
+
+    }
+
+
+    private void callPrint2D(I2DDocument mockedDocument, boolean isPolarCoordinates, WallType shapeType, int y2, int x2) {
+        int pictureHeight = 100;
+        int pictureWidth = 200;
+        int resolutionX = 7;
+        int resolutionY = 7;
+        ShapeContext sc = new ShapeContext(isPolarCoordinates, pictureHeight, pictureWidth);
+
+        int y1 = 1;
+        int x1 = 0;
+
+        when(mockedDocument.getContext()).thenReturn(sc);
+
+        WallShape w = new WallShape(777, shapeType, new Point2DInt(x1, y1), new Point2DInt(x2, y2), 99,99);
+
+        IPrintStyle mockedPrintStyle = mock(IPrintStyle.class);
+        when(mockedPrintStyle.getInnerWallColor()).thenReturn(new Color("040404"));
+        when(mockedPrintStyle.getOuterWallColor()).thenReturn(new Color("050505"));
+        when(mockedPrintStyle.getSolutionWallColor()).thenReturn(new Color("060606"));
+        when(mockedPrintStyle.getDebugWallColor()).thenReturn(new Color("070707"));
+        when(mockedPrintStyle.getInnerWallWidth()).thenReturn(3);
+        when(mockedPrintStyle.getOuterWallWidth()).thenReturn(5);
+        when(mockedPrintStyle.getSolutionWallWidth()).thenReturn(7);
+
+        w.print2D(mockedDocument, mockedPrintStyle);
+    }
+
+    @Test
+    public void testGetX1() {
+        WallShape w = new WallShape(0, WallType.innerWall, new Point2DInt(11, 22), new Point2DInt(33, 44), 3, 4);
+        assertEquals(11, w.getX1());
+    }
+
+    @Test
+    public void testGetX2() {
+        WallShape w = new WallShape(0, WallType.innerWall, new Point2DInt(11, 22), new Point2DInt(33, 44), 3, 4);
+        assertEquals(33, w.getX2());
+    }
+
+    @Test
+    public void testGetY1() {
+        WallShape w = new WallShape(0, WallType.innerWall, new Point2DInt(11, 22), new Point2DInt(33, 44), 3, 4);
+        assertEquals(22, w.getY1());
+    }
+
+    @Test
+    public void testGetY2() {
+        WallShape w = new WallShape(0, WallType.innerWall, new Point2DInt(11, 22), new Point2DInt(33, 44), 3, 4);
+        assertEquals(44, w.getY2());
+    }
+
+
 }
